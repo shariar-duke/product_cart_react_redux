0898f120cffcf881afeb0dcec5a7ab1c003c42bf
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-15 20:26:49 +0600

    Cart comopnent with PriceBox and CartItem

--- src/comonents/Cart.jsx
+++ src/comonents/Cart.jsx
@@ -1,8 +1,19 @@
-
+import CartItem from "./CartItem";
+import PriceBox from "./PriceBox";
 export default function Cart() {
   return (
-    <div>
-      Cart Component
+    <div className="mt-[120px] px-[30px] grid grid-cols-3 gap-[36px]">
+      <div className="col-span-2">
+        <p className="font-bold text-[20px] mb-[26px]">Shopping Cart</p>
+        <div className="flex flex-col gap-[16px]">
+          <CartItem />
+          <CartItem />
+          <CartItem />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <PriceBox />
+      </div>
     </div>
-  )
+  );
 }

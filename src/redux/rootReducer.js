69d029082d740f4a48383e9070066e7f52547dec
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 09:46:30 +0600

    roote Reducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import cartReducer from "./cartList/reducer";
 import productReducer from "./productList/reducer";
 
 
 const rootReducer = combineReducers({
-    product : productReducer
+    product : productReducer,
+    cart : cartReducer
 })
 
 export default rootReducer;

179fca26b737713328df06e28c1d786116b1f1d2
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 10:33:07 +0600

    reducer function of product

--- src/redux/productList/reducer.js
+++ src/redux/productList/reducer.js
@@ -1,32 +1,57 @@
-import { ADDED } from "./actionTypes";
+import { ADDED, DECREASE, INCREASE } from "./actionTypes";
 import { initialState } from "./initialState";
 
-
-// write a function for next productId
-
-function nextProductId(products) 
-{
-    const maxId = products.reduce((firstProduct, product) => Math.max(firstProduct , product.id) ,-1 )
+// Function to calculate the next product ID
+function nextProductId(products) {
+    const maxId = products.reduce((max, product) => Math.max(max, product.id), -1);
     return maxId + 1;
 }
 
-const reducer = (state = initialState , action) => 
-{
-    switch(action.type) 
-    {
+// Function to find the index of a product by ID
+function findIndex(state, productId) {
+    return state.findIndex((product) => product.id === productId);
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case ADDED:
             return [
                 ...state, 
-                {   ...action.payload,
-                    id:nextProductId(state),
-                    
+                {
+                    ...action.payload,
+                    id: nextProductId(state),
                 }
-            ]
+            ];
+
+        case INCREASE: {
+            const index = findIndex(state, action.payload);
+            if (index !== -1) {
+                const newState = [...state];
+                newState[index] = {
+                    ...newState[index],
+                    quantity: state[index].quantity + 1
+                };
+                return newState;
+            }
+            return state;
+        }
+
+        case DECREASE: {
+            const index = findIndex(state, action.payload);
+            if (index !== -1) {
+                const newState = [...state];
+                newState[index] = {
+                    ...newState[index],
+                    quantity: state[index].quantity -1
+                };
+                return newState;
+            }
+            return state;
+        }
+
         default:
-            return state
-
+            return state;
     }
-}
-
+};
 
 export default reducer;

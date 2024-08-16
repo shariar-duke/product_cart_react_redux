d8aba3d38d95920571a2d221052d876781208d90
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 06:38:18 +0600

    provide the store in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { Provider } from "react-redux"
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Cart from "./comonents/Cart"
 import Header from "./comonents/Header"
 import Home from "./comonents/Home"
+import store from "./redux/store"
 export default function App() {
   return (
-    <Router>
+
+    <Provider store={store}>
+       <Router>
       <Header/>
      <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/cart" element={<Cart/>}  />
      </Routes>
     </Router>
+    </Provider>
+   
   )
 }

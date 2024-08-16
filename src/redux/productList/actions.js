cbe32a33e19e135a110618a543dfc3dde4d71fd0
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 10:32:27 +0600

    product actions

--- src/redux/productList/actions.js
+++ src/redux/productList/actions.js
@@ -1,11 +1,37 @@
-import { ADDED } from "./actionTypes";
+import { ADDED, DECREASE, INCREASE, RESTORED } from "./actionTypes";
 
-// This is for add product 
+// This is for add product
 
 export const added = (product) => {
+  return {
+    type: ADDED,
+    payload: product,
+  };
+};
 
+export const increase = (productId) => {
     return {
-       type : ADDED, 
-       payload: product
+        type : INCREASE,
+        payload : productId
+    }
+};
+
+
+export const decrease =(productId) => 
+{
+    return {
+        type : DECREASE,
+        payload : productId
+    }
+}
+
+export const restored =(productId, numberOfProduct)=> 
+{
+    return {
+        type: RESTORED,
+        payload: {
+          productId,
+          numberOfProduct
+        }
     }
 }

fb399e97b0072926381fad79b4d6346592ca1550
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 06:40:11 +0600

    Fetch the product data and use ProductCart component

--- src/comonents/Home.jsx
+++ src/comonents/Home.jsx
@@ -1,18 +1,20 @@
+import { useSelector } from "react-redux";
 import AddProduct from "./AddProduct";
 import ProductCart from "./ProductCart";
 
 export default function Home() {
+  const products = useSelector((state) => state.product);
+
   return (
     <div className="mt-[120px] px-[30px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[20px]">
       <div className="md:col-span-1 lg:col-span-1">
         <AddProduct />
       </div>
-      
-      <div className="md:col-span-1 lg:col-span-2 grid md:grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-[20px]">
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
-        <ProductCart />
+
+      <div className="md:col-span-1 lg:col-span-2 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-[20px]">
+        {products.map((singleProduct) => (
+          <ProductCart key={singleProduct.id} product={singleProduct} />
+        ))}
       </div>
     </div>
   );
--- src/comonents/ProductCart.jsx
+++ src/comonents/ProductCart.jsx
@@ -1,14 +1,21 @@
-export default function ProductCart() {
+/* eslint-disable react/prop-types */
+ 
+export default function ProductCart({product}) {
+
+
+   const backgroundImageStyle = {
+     backgroundImage:`url(${product.imgUrl})`
+   }
   return (
-    <div className="shadow-2xl rounded-md">
-     <div className="h-[160px] max:w-[280px] bg-[url('/bgImg.jpg')] bg-cover bg-center rounded-md "></div>
+    <div className="shadow-2xl rounded-md  max-h-[330px]">
+     <div className="h-[160px] max:w-[280px]  bg-cover bg-center rounded-md" style={backgroundImageStyle}></div>
 
       <div className="mt-[30px] p-[10px]">
-        <p className="text-[16px] font-bold">Apple Macbook</p>
-        <span className="text-gray-500 text-[14px]">Laptop</span>
+        <p className="text-[16px] font-bold">{product?.productName}</p>
+        <span className="text-gray-500 text-[14px]">{product?.category}</span>
         <div className="flex justify-between w-full ">
-          <p className="text-[14px] font-bold">BDT 12000</p>
-          <p className="text-[14px] font-bold">QTY 20 </p>
+          <p className="text-[14px] font-bold">BDT {product?.price}</p>
+          <p className="text-[14px] font-bold">QTY {product?.quantity} </p>
         </div>
         <button className="text-white text-[14px] font-bold text-lg mt-2 bg-gray-800 hover:bg-gray-900 active:bg-black w-full py-[4px] rounded-md shadow-md cursor-pointer transition-all duration-200">
           Add Product

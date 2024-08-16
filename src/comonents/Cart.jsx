9d864e984a795e5dda2147d5a5115567a1d6e45e
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 09:48:34 +0600

    dynamic values of caartItem

--- src/comonents/Cart.jsx
+++ src/comonents/Cart.jsx
@@ -1,14 +1,16 @@
+import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import PriceBox from "./PriceBox";
 export default function Cart() {
+  const allCarts = useSelector((state) => state.cart);
+  console.log("All the carts are", allCarts);
   return (
     <div className="mt-[120px] px-[30px] grid grid-cols-3 gap-[36px]">
       <div className="col-span-2">
         <p className="font-bold text-[20px] mb-[26px]">Shopping Cart</p>
         <div className="flex flex-col gap-[16px]">
-          <CartItem />
-          <CartItem />
-          <CartItem />
+          {allCarts &&
+            allCarts.map((cart) => <CartItem key={cart.id} cart={cart} />)}
         </div>
       </div>
       <div className="col-span-1">
--- src/comonents/CartItem.jsx
+++ src/comonents/CartItem.jsx
@@ -1,19 +1,25 @@
+/* eslint-disable react/prop-types */
 
 import { RiDeleteBin6Line } from "react-icons/ri";
-export default function CartItem() {
+export default function CartItem({cart}) {
+
+    const backgroundImageStyle = {
+        backgroundImage: `url(${cart?.imgUrl ?? ""})`,
+      };
+    
   return (
     <div className="border border-gray-200 shadow-lg rounded-lg flex  p-[14px]  items-center">
        <div className="flex flex-1 gap-[12px]">
-        <div className="h-[80px] w-[140px] rounded-md bg-cover bg-center bg-[url('/bgImg.jpg')]"></div>
+        <div className="h-[80px] w-[140px] rounded-md bg-cover bg-center" style={backgroundImageStyle}></div>
          <div className="flex flex-col gap-[5px]  h-full">
-            <p className="text-[16px] font-bold text-black-200">Apple Laptop</p>
-            <p className="text-[14px] font-bold text-gray-600">Apple Laptop</p>
-            <p className="text-[16px] font-bold text-gray">BDT 20000</p>
+            <p className="text-[16px] font-bold text-black-200">{cart?.productName}</p>
+            <p className="text-[14px] font-bold text-gray-600"> {cart?.category} </p>
+            <p className="text-[16px] font-bold text-gray">{cart?.price}</p>
          </div>
        </div>
        <div className="flex flex-1  gap-[14px]">
         <p className="text-[20px] font-medium cursor-pointer">+</p>
-        <p className="text-[20px] font-medium cursor-pointer">2</p>
+        <p className="text-[20px] font-medium cursor-pointer">{cart?.quantity}</p>
         <p className="text-[20px] font-medium cursor-pointer">-</p>
        </div>
        <RiDeleteBin6Line className="cursor-pointer" color="red"  size={26}/>

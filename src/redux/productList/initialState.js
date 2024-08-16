b5504eb4387648df7e7b533276ffd1ec9883d799
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 06:33:46 +0600

    initial state

---
+++ src/redux/productList/initialState.js
@@ -0,0 +1,10 @@
+export const initialState = [
+    {
+        id: 1,
+        productName: "Samsung G7",
+        category: "Phone",
+        price: 2000,
+        quantity: 12,
+        imgUrl: "https://images.unsplash.com/photo-1512820790803-83ca734da794" // Laptop image URL
+    }
+];

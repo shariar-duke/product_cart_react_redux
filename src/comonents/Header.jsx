94c83fa9016511307a0d11822daa936e14de36c5
Shariar Duke <shariarmahmud225@gmail.com>
2024-08-16 14:17:00 +0600

    Changes in Header and End of this Task

--- src/comonents/Header.jsx
+++ src/comonents/Header.jsx
@@ -1,6 +1,11 @@
 import { CiShoppingCart } from "react-icons/ci";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 export default function Header() {
+
+  const allCarts = useSelector((state) => state.cart);
+  const totalProduct = allCarts.reduce((sum, product) => sum + product.quantity ,0)
+
   return (
     <div className="fixed top-0 w-full py-[24px] px-[40px] bg-[#272727] flex justify-between">
       <img src="/logo.png" alt="logo" className="h-[40px] w-[40px]" />
@@ -15,7 +20,7 @@ export default function Header() {
         <Link to="/cart">
           <div className="flex justify-center items-center mr-[30px] cursor-pointer">
             <CiShoppingCart size={28} color="white" />
-            <p className="text-white  font-bold text-[16px]">2</p>
+            <p className="text-white  font-bold text-[16px]">{totalProduct?? 0}</p>
           </div>
         </Link>
 
